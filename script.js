ecebf666c19baa576b3237dce06b768dcea8ee4c
rumesh <rumeshk066@gmail.com>
2024-09-30 10:21:00 +0530

    rest

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const arr1 = [1, 2, 3];
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function sum(a, b, c){
-  console.log(a + b + c);
+function sum(...args) {
+  let sum = 0;
+  for (const arg of args) {
+    sum += arg;
+  }
+  console.log(sum);
 }
 
 sum(...arr1);
