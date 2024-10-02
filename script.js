c848b51b23a99a894efba0229ba6e83912c37722
rumesh <rumeshk066@gmail.com>
2024-10-02 16:24:12 +0530

    add_1

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 class rectangle {
-  constructor(_width, _height, _color){
+  constructor(_width, _height){
     this.width = _width;
     this.height = _height;
-    this.color = _color;
-    console.log("construct");
+    console.log(`we are using ${this.width} and ${this.height}`);
   }
 
-  gerArea(){
-    return this.width * this.height;
-  }
-  static calculate(a, b){
-    return a * b;
-  } 
+  
 }
 
 const re = new rectangle(5, 6);
 
-console.log(re.gerArea()); // Output: 10
-console.log(rectangle.calculate(5, 6)); // Output:
