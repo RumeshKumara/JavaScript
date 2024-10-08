29ad17a089753f214a13a24a4f543b0d71f21275
rumesh <rumeshk066@gmail.com>
2024-10-08 10:13:55 +0530

    Add challenge

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-class Car {
-  constructor(name, year) {
-    this.name = name;
-    this.year = year;
+var score1, score2, score3, averageSri, averageEng;
 
-  }
-  age() {
-    const date = new Date();
-    return date.getFullYear() - this.year;
-  }
+// Sri Lanka average
+score1 = parseInt(prompt("First match score of team Sri Lanka : "));
+score2 = parseInt(prompt("Second match score of team Sri Lanka : "));
+score3 = parseInt(prompt("Third match score of team Sri Lanka : "));
+
+averageSri = (score1+score2+score3)/3;
+
+// England average
+score1 = parseInt(prompt("First match score of team England : "));
+score2 = parseInt(prompt("Second match score of team England : "));
+score3 = parseInt(prompt("Third match score of team England : "));
+
+averageEng = (score1+score2+score3)/3;
+
+// Find the winner
+if(averageSri > averageEng){
+    alert("Winner is team Sri Lanka | Average Score : "+averageSri);
+    console.log("Winner is team Sri Lanka | Average Score : "+averageSri);
 }
-
-const myCar = new Car("Ford", 2009);
-document.getElementById("demo").innerHTML =
-"My car is " + myCar.age() + " years old.";
+else if(averageSri < averageEng){
+    alert("Winner is team England | Average Score : "+averageEng);
+    console.log("Winner is team England | Average Score : "+averageEng);
+}
+else{
+    alert("Match drawn, average scores are equal!");
+    console.log("Match drawn, average scores are equal!");
+}
